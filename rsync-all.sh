13ee53d58b7e5150c0fc7858314a6a6b12e39a10
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-12-16 13:57:49 -0600

    update rsync to deploy to private sandbox
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- rsync-all.sh
+++ rsync-all.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-echo "Pushing to scalaworkshop.cs.luc.edu"
+echo "Pushing to packt.cs.luc.edu"
 rsync -avz --exclude .htaccess \
   $(pwd)/build/ \
-  scalaworkshop.cs.luc.edu:/var/www/vhosts/scalaworkshop.cs.luc.edu/htdocs/
-echo
-echo "Pushing to introcs.cs.courseclouds.com"
-rsync -avz --exclude .htaccess \
-  $(pwd)/build/ \
-  scalaworkshop.com:./public_html/
+  packt.cs.luc.edu:/var/www/vhosts/packt.cs.luc.edu/htdocs/
 echo
